f6b652fab8ecb90c99622f5c588a40386b447075
brubsbt-lab <brubs.bt@gmail.com>
2026-07-04 12:17:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,87 +10,77 @@ async function buscarProduto() {
     .split("\n")
     .map(l => l.trim())
     .filter(l => l !== "");
-
-produtos = [];        
         
-for (const url of urls) {
+        produtos = [];        
         
-    if (!url) {
-        alert("Cole o link do produto.");
-        return;
-    }
-
-    try {
-
-        const resposta = await fetch(
-            "https://hook.us2.make.com/9h9tic2oo03ftyghkomkjyu90if73i4w",
-            {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    url: url
-                })
-            }
-        );
-
-        const dados = await resposta.json();
-
-        const nomeLimpo = dados.nome
-            .replace(" - Compre Agora | Dafiti Brasil", "")
-            .trim();
-
-        document.getElementById("nome").innerHTML = nomeLimpo;
-
-        document.getElementById("original").innerHTML =
-            `De: R$ ${dados.precoOriginal}`;
-
-        document.getElementById("atual").innerHTML =
-            `Por: R$ ${dados.precoAtual}`;
-
-        document.getElementById("imagem").src = dados.imagem;
-        document.getElementById("imagem").style.display = "block";
-
-        const original = parseFloat(dados.precoOriginal);
-        const atual = parseFloat(dados.precoAtual);
-
-        const desconto = Math.round(
-            ((original - atual) / original) * 100
-        );
-
-        document.getElementById("desconto").innerHTML =
-            `🔥 ${desconto}% OFF`;
-
-        document.getElementById("descricao").value =
-                `🔥 OFERTA
-                ${nomeLimpo}
-                🔥 ${desconto}% OFF
-                💰 De R$ ${dados.precoOriginal}
-                ✅ Por apenas R$ ${dados.precoAtual}
-                🛍️ Compre aqui:
-                ${url}
-                🏃 Aproveite antes que acabe!`;
-
-        produtoAtual = {
-                url: url,
-                nome: nomeLimpo,
-                precoOriginal: dados.precoOriginal,
-                precoAtual: dados.precoAtual,
-                imagem: dados.imagem,
-                desconto: desconto
-        };
-        produtos.push(produtoAtual);
-
-    } catch (erro) {
-
-        console.error(erro);
-
-    }
-
+        for (const url of urls) {
+                if (!url) {
+                        alert("Cole o link do produto.");
+                        return;
+                }
+                try {
+                        const resposta = await fetch(
+                                "https://hook.us2.make.com/9h9tic2oo03ftyghkomkjyu90if73i4w",
+                                {
+                                        method: "POST",
+                                        headers: {
+                                                "Content-Type": "application/json"
+                                        },
+                                        body: JSON.stringify({
+                                                url: url
+                                        })
+                                }
+                        );
+                        const dados = await resposta.json();
+                        const nomeLimpo = dados.nome
+                                .replace(" - Compre Agora | Dafiti Brasil", "")
+                                .trim();
+                        
+                        document.getElementById("nome").innerHTML = nomeLimpo;
+                        
+                        document.getElementById("original").innerHTML =
+                                `De: R$ ${dados.precoOriginal}`;
+                        
+                        document.getElementById("atual").innerHTML =
+                                `Por: R$ ${dados.precoAtual}`;
+                        
+                        document.getElementById("imagem").src = dados.imagem;
+                        document.getElementById("imagem").style.display = "block";
+                        
+                        const original = parseFloat(dados.precoOriginal);
+                        const atual = parseFloat(dados.precoAtual);
+                        const desconto = Math.round(
+                                ((original - atual) / original) * 100
+                        );
+                        
+                        document.getElementById("desconto").innerHTML =
+                                `🔥 ${desconto}% OFF`;
+                        
+                        document.getElementById("descricao").value =
+                                `🔥 OFERTA
+                                ${nomeLimpo}
+                                🔥 ${desconto}% OFF
+                                💰 De R$ ${dados.precoOriginal}
+                                ✅ Por apenas R$ ${dados.precoAtual}
+                                🛍️ Compre aqui:
+                                ${url}
+                                🏃 Aproveite antes que acabe!`;
+                        
+                        produtoAtual = {
+                                url: url,
+                                nome: nomeLimpo,
+                                precoOriginal: dados.precoOriginal,
+                                precoAtual: dados.precoAtual,
+                                imagem: dados.imagem,
+                                desconto: desconto
+                        };
+                        produtos.push(produtoAtual);
+                } catch (erro) {
+                        console.error(erro);
+                }
+        }
+        alert(`${produtos.length} produto(s) encontrado(s)!`);
 }
-
-alert(`${produtos.length} produto(s) encontrado(s)!`);
 
 async function publicarTelegram(){
 
